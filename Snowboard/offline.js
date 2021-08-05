b2ec04ebce3b97063019fa3a31d80b9fc3292610
Aditya Yuwono <>
2021-08-05 17:50:19 +0700

    Publish Snowboard

---
+++ Snowboard/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1628160393,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/leftbutton.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/ski_depan-sheet0.png",
+		"images/ski_depan-sheet1.png",
+		"images/tiledbackground.png",
+		"images/laneguide.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
