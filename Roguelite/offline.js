7534066c58259d6e4dcf35dee9a0c1ed0a10f162
Aditya Yuwono <aditya.yuwono.3d@gmail.com>
2021-08-09 19:50:39 +0700

    Roguelite: Optimize asset for chrome

--- Roguelite/offline.js
+++ Roguelite/offline.js
@@ -1,9 +1,9 @@
 ﻿{
-	"version": 1628497733,
+	"version": 1628511424,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
-		"jquery-2.1.1.min.js",
+		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/jump-sheet0.png",
 		"images/moveleft-sheet0.png",
